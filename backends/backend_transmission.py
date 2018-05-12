f377f4f502e670bac74f2e2e88588f601b7122b0
Marcin Kocot <me@foo.bz>
2018-05-13 00:40:59 +0200

    Change default useragent

--- backends/backend_transmission.py
+++ backends/backend_transmission.py
@@ -1,12 +1,19 @@
 import transmissionrpc
 from backends import Backend, Torrent
-
+import urllib
 
 class BackendTransmission(Backend):
     def __init__(self, conf):
         self.client = transmissionrpc.Client(
             address=conf['server']['address'],
             port=int(conf['server']['port']))
+
+        # replace default name to something sane as we might get bounced
+        # with default values. This is 'ugly' way but
+        #  library soesn't allow to change it gently
+        proper_opener = urllib.request.build_opener()
+        proper_opener.addheaders = [('User-agent', 'transmission-rss/0.0 [python]')]
+        urllib.request.install_opener(proper_opener)
 
     def add_torrent(self, address, paused=True):
         torrent = self.client.add_torrent(address, paused=paused)
